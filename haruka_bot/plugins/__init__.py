6eeb83af31929ee07bc18ca0aae41910c1ef0be8
lue-trim <leavesleviathan@outlook.com>
2024-10-10 15:09:33 +0800

    init导入login.py

--- haruka_bot/plugins/__init__.py
+++ haruka_bot/plugins/__init__.py
@@ -6,4 +6,4 @@ from .dynamic import dynamic_off, dynamic_on  # noqa: F401
 from .permission import permission_off, permission_on  # noqa: F401
 from .pusher import dynamic_pusher, live_pusher  # noqa: F401
 from .live import live_off, live_on, live_now  # noqa: F401
-from .sub import add_sub, delete_sub, sub_list  # noqa: F401
+from .sub import add_sub, delete_sub, sub_list, login  # noqa: F401
--- haruka_bot/plugins/sub/login.py
+++ haruka_bot/plugins/sub/login.py
@@ -1,9 +1,5 @@
-from bilireq.exceptions import ResponseCodeError
-from bilireq.user import get_user_info
 from bilireq.auth import WebAuth
 from nonebot.adapters.onebot.v11.event import MessageEvent
-from nonebot.params import ArgPlainText
-from nonebot_plugin_guild_patch import GuildMessageEvent
 
 from pathlib import Path
 
@@ -19,10 +15,10 @@ from ...utils import (
     get_path,
 )
 
-login_action = on_command("登录", aliases={"登录B站"}, rule=to_me(), priority=5)
+login_action = on_command("登录", rule=to_me(), priority=5)
 login_action.__doc__ = """登录 sessdata"""
 
-login_action.handle()(permission_check)
+login_action.handle()
 
 login_action.got("uid", prompt="请输入sessdata")
 
