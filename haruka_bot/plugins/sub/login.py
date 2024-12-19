539ddd77a063e11a7309c42dd2b17dd34fc0103c
lue-trim <leavesleviathan@outlook.com>
2024-12-19 14:05:45 +0800

    删除对话框登录/退出登录功能

--- haruka_bot/plugins/sub/login.py
+++ haruka_bot/plugins/sub/login.py
@@ -16,7 +16,7 @@ from ...utils import (
 )
 
 login_action = on_command("登录", rule=to_me(), priority=5)
-login_action.__doc__ = """登录 <cookies> \
+login_action.__doc__ = """!该接口已弃用 登录 <cookies> \
     （用于B站相关API鉴权，cookies格式需为包含sessdata/dedeuserid/ac_time_value/bili_jct几个字段的json）"""
 
 login_action.handle()
