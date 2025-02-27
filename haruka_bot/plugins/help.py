0b0933e82c5ac906738f069497f7c6c4da8797fa
lue-trim <leavesleviathan@outlook.com>
2025-02-27 17:53:14 +0800

    1.7.3 更新使用说明

--- haruka_bot/plugins/help.py
+++ haruka_bot/plugins/help.py
@@ -8,7 +8,14 @@ help = on_command("帮助", rule=to_me(), priority=5)
 
 @help.handle()
 async def _():
-    message = "HarukaBot目前支持的功能：\n（请将UID替换为需要操作的B站UID）\n"
+    message = ""
+    message += """\
+快速使用：
+使用"/关注 <UID>"添加想要推送的主播，使用"/关注列表"确认推送开关的状态
+私人订制：
+可以点击机器人头像私信并发送操作指令，私信内容与大屏推送完全独立
+"""
+    message += "完整功能列表：\n"
     for matchers_list in matchers.values():
         for matcher in matchers_list:
             if (
@@ -17,5 +24,5 @@ async def _():
                 and matcher.__doc__
             ):
                 message += matcher.__doc__ + "\n"
-    message += f"\n当前版本：v{__version__}\n" "详细帮助：https://haruka-bot.sk415.icu/usage/"
+    message += f"\n当前版本：HarukaBot修改版 {__version__}\n" #"详细帮助：https://haruka-bot.sk415.icu/usage/"
     await help.finish(message)
